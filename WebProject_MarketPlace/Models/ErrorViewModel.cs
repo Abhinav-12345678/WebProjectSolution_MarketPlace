493f1607c1f5b750b5b0fcf500c9b3bd527f10b9
ABHINAV <ABHINAV@LAPTOP-AG6LL44L>
2023-10-30 12:22:22 +0530

    Add project files.

---
+++ WebProject_MarketPlace/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebProject_MarketPlace.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
